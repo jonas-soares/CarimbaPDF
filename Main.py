e8b0b08190b01fd1afcdf8b8a47ebca59e614774
Jonas Soares <93543455+jonas-soares@users.noreply.github.com>
2022-07-18 16:40:12 -0300

    Ajustes de modularizacao.

--- Main.py
+++ Main.py
@@ -1,1 +1,6 @@
+from funcoes import *
 
+a = selecionaArquivos()
+
+# a retorna uma tupla e nao uma string. tem que converter para b funcionar
+b = tamanhoPagina(a)
--- funcoes.py
+++ funcoes.py
@@ -1,14 +1,14 @@
 from struct import pack
 from tkinter import *
 from tkinter import filedialog
+from PyPDF2 import PdfFileReader
 
 def selecionaArquivos():
     caminhoArquivo = filedialog.askopenfilenames()
     print(caminhoArquivo)
     return(caminhoArquivo)
     
+def tamanhoPagina(arquivo):
+    input1 = PdfFileReader(open(arquivo, 'rb'))
+    return(input1.getPage(0).mediaBox)
 
-window = Tk()
-botao = Button(text="Selecionar Arquivos", command=selecionaArquivos)
-botao.pack()
-window.mainloop()
