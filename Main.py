b7bda13136abf01f987887111c7010b520ea36bb
Jonas Soares <93543455+jonas-soares@users.noreply.github.com>
2022-07-29 13:13:25 -0300

    Verificação da tupla para string em Main.

--- Main.py
+++ Main.py
@@ -1,6 +1,9 @@
 from funcoes import *
 
 a = selecionaArquivos()
-
+print(a)
+print(a[0])
+print(a[1])
 # a retorna uma tupla e nao uma string. tem que converter para b funcionar
 b = tamanhoPagina(a)
+print(b)
